40f846e8f9ac0be5816e3e4063627ac93e06bbe7
RINO <779057748@qq.com>
2021-01-15 15:37:21 +0800

    init

---
+++ char.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  //char ch='A';
+  //printf("%c\n",ch);
+  //printf("%d\n",20);
+  //int age=20;
+  //printf("%d",age);
+  printf("%d\n",sizeof(char));
+  printf("%d\n",sizeof(short));
+  printf("%d\n",sizeof(int));
+  printf("%d\n",sizeof(long));
+  printf("%d\n",sizeof(long long));
+  printf("%d\n",sizeof(float));
+  printf("%d\n",sizeof(double));
+  return 0;
+}
---
+++ test.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()//主函数
+{
+  printf("hehe来来来\n");
+  return 0;
+}
