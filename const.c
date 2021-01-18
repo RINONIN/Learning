edc782c03241a786531ade6a49b74c84a6417371
RINO <779057748@qq.com>
2021-01-18 20:41:39 +0800

    init

---
+++ const.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int n=10;
+  int arr[10]={0};
+  printf("%d\n",arr);
+  //int num=4;
+  //printf("%d\n",num);
+  //num=8;
+  //printf("%d\n",num);
+  return 0;
+}
---
+++ defin.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  #define max 10 // no ";"
+int main()
+{
+  int arr[max]={0};
+  printf("%d\n",max);
+  return 0;
+
+}
