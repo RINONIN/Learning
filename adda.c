7d2f6f8b1150ef815becb559826c3021c2cb4041
RINO <779057748@qq.com>
2021-01-18 15:49:21 +0800

    init

---
+++ adda.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()//主函数
+{
+  int a=0;
+  int b=0;
+  int sum;
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("sum=%d\n",sum);
+  return 0;
+}
--- short.c
+++ short.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+int a=100;
 int main()
 {
-float weight=95.6;
-return 0;
+  {int a=20;}
+  printf("%d\n",a);
+//float weight=95.6;
+//return 0;
 }
