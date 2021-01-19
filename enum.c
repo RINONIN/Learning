4b6b5f8cd983f7f7249d73953cb15c8c2896e89f
RINO <779057748@qq.com>
2021-01-19 19:30:33 +0800

    init

---
+++ enum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+ int main()
+enum gender
+{
+  male,
+  female,
+  secret
+};
+ {
+   enum gender bob=male;
+  printf("%d\n",bob);
+  printf("%d\n",male);
+  printf("%d\n",female);
+  printf("%d\n",secret);
+  return 0;
+ }
