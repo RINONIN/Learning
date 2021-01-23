40ab4f5c9c1f72cc5c49cb59b8cb70f96ce6165b
RINO <779057748@qq.com>
2021-01-23 16:50:15 +0800

    init

---
+++ trans.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  printf("c:\\test\\32\\test.c\n");
+  printf("(are you ok??)\n");
+  printf("\"\n");
+  printf("%c\n",'2');
+  printf("%s\n","aaaaa");
+  printf("%d\n",strlen("c:\test\32\test.c"));
+  printf("%s\n","c:\test\32\test.c");
+  printf("%c\n",'\x25');
+  return 0 ;
+} 
