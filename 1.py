d1db5c093166c2cc1d09197b8e11e1cbc5f035ad
RINO <779057748@qq.com>
2021-01-26 16:12:15 +0800

    init

---
+++ 1.py
@@ -0,0 +1,2 @@
+str='C:\now'
+print(str)
---
+++ stringname.py
@@ -0,0 +1,26 @@
+teacher='lsp'
+print(teacher)
+first=3
+second=8
+third=first+second
+print(third)
+myteacher='lsp'
+yourteacher='dsb'
+ourteacher=myteacher+yourteacher
+print(ourteacher)
+
+#first
+#5+8
+#'5'+'8'
+#'let\'s go'
+#原始字符串，前面加r
+strqa=r'C:\s\s\s\s\s\s'
+print(strqa)
+#三重引号字符串"
+strr="""
+sdvhhjdksjdhdksdn
+dvjsjg
+sdhufsihfufe
+snjfksenfj
+skfsjfo"""
+print(strr)
