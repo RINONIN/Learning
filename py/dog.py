e1ca757561f5f936ef5071807645e85eebcd03af
RINONIN <779057748@qq.com>
2022-04-09 19:20:17 +0800

    init

--- py/dog.py
+++ py/dog.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
  
-age = int(input("请输入你家狗狗的年龄: "))
-print("")
-if age <= 0:
-    print("你是在逗我吧!")
-elif age == 1:
-    print("相当于 14 岁的人。")
-elif age == 2:
-    print("相当于 22 岁的人。")
-elif age > 2:
-    human = 22 + (age -2)*5
-    print("对应人类年龄: ", human)
+# age = int(input("请输入你家狗狗的年龄: "))
+# print("")
+# if age <= 0:
+#     print("你是在逗我吧!")
+# elif age == 1:
+#     print("相当于 14 岁的人。")
+# elif age == 2:
+#     print("相当于 22 岁的人。")
+# elif age > 2:
+#     human = 22 + (age -2)*5
+#     print("对应人类年龄: ", human)
  
-### 退出提示
+# ### 退出提示
+# input("点击 enter 键退出")
+
+print("=======欢迎进入狗狗年龄对比系统========")
+while True:
+    try:
+        age = int(input("请输入您家狗的年龄:"))
+        print(" ")
+        age = float(age)
+        if age < 0:
+            print("您在逗我？")
+        elif age == 1:
+            print("相当于人类14岁")
+            break
+        elif age == 2:
+            print("相当于人类22岁")
+            break
+        else:
+            human = 22 + (age - 2)*5
+            print("相当于人类：",human)
+            break
+    except ValueError:
+        print("输入不合法，请输入有效年龄")
+###退出提示
 input("点击 enter 键退出")
