ee153213fbfd04c091b0cf8b1d8b9ee63402b5e4
RINONIN <779057748@qq.com>
2022-04-28 10:27:44 +0800

    init

--- py/99.py
+++ py/99.py
@@ -12,3 +12,10 @@
 
 
 print(sum(range(101)))
+
+for i in range(9,0,-1):
+    for j in range (1,i):
+        print("\t",end="")
+    for k in range (i,10):
+        print("%dx%d=%d" % (i,k,k*i), end="\t")
+    print()
